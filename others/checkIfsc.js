00bce5a749f0bd408dd4d5e93e8bc9a13a0d87c6
Harsh <harsh.gupta@cognustechnology.com>
2024-09-17 12:10:58 +0530

    api changes

--- others/checkIfsc.js
+++ others/checkIfsc.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  connectionLimit: 20,
 });
-
 
 router.get("/:ifsc", async (req, res) => {
   const ifscCode = req.params.ifsc.toUpperCase();
