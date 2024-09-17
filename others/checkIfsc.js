59c74a5bb5efd716efa90f0fae0081ddc7d61104
Harsh <harsh.gupta@cognustechnology.com>
2024-09-17 12:15:34 +0530

    logs added

--- others/checkIfsc.js
+++ others/checkIfsc.js
@@ -22,6 +22,7 @@ router.get("/:ifsc", async (req, res) => {
 
   try {
     const connection = await pool.getConnection();
+    console.log("connection created");
 
     if (all === "all") {
       const [data] = await connection.query("SELECT * FROM banks");
@@ -35,6 +36,9 @@ router.get("/:ifsc", async (req, res) => {
       });
       res.json({ links });
     } else {
+    console.log("connection is in else");
+    console.log("data fetched start");
+
       const [rows] = await connection.query(
         "SELECT Bank, Ifsc, City2, State FROM banks WHERE Ifsc LIKE ?",
         [`${ifscCode}%`]
@@ -42,6 +46,8 @@ router.get("/:ifsc", async (req, res) => {
       console.log(rows);
 
       res.json(rows);
+    console.log("data fetched end");
+
     }
   } catch (err) {
     res.status(500).json({ message: err.message });
